21ee6cf6091f6286d1089feffd83919209da6cbd
srkarthi1982 <srkarthi1982@gmail.com>
2025-12-02 11:55:34 +0400

    added layouts

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
-// Do not write code directly here, instead use the `src` folder!
-// Then, use this file to export everything you want your user to access.
-
+export { default as WebLayout } from "./src/layouts/WebLayout.astro";
+export { default as AuthLayout } from "./src/layouts/AuthLayout.astro";
 export { default as AvBrand } from './src/AvBrand.astro';
 export { default as AvButton } from './src/AvButton.astro';
 export { default as AvCard } from './src/AvCard.astro';
@@ -14,7 +13,4 @@ export { default as AvInput } from './src/AvInput.astro';
 export { default as AvNavbar } from './src/AvNavbar.astro';
 export { default as AvNavbarActions } from './src/AvNavbarActions.astro';
 export { default as AvTimeline } from './src/AvTimeline.astro';
-export { default as MyComponent } from './src/MyComponent.astro';
 
-// Preserve default export for backwards compatibility.
-export { default } from './src/MyComponent.astro';
